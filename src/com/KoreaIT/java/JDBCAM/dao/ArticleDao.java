57ce160d839ba5b23a9ed22d2bc52da082056e18
yhwit30 <yhwit30@gmail.com>
2024-01-10 17:51:30 +0900

    article write 리팩토링

--- src/com/KoreaIT/java/JDBCAM/dao/ArticleDao.java
+++ src/com/KoreaIT/java/JDBCAM/dao/ArticleDao.java
@@ -6,10 +6,28 @@ import com.KoreaIT.java.JDBCAM.util.DBUtil;
 import com.KoreaIT.java.JDBCAM.util.SecSql;
 
 public class ArticleDao {
+	
+	private Connection conn;
 
-	public int dbInsert(Connection conn, SecSql sql) {
+	public ArticleDao(Connection conn) {
+		this.conn = conn;
+	}
+
+	
+	public int doWrite(String title, String body) {
+		
+		SecSql sql = new SecSql();
+
+		sql.append("INSERT INTO article");
+		sql.append("SET regDate = NOW(),");
+		sql.append("updateDate = NOW(),");
+		sql.append("title = ?,", title);
+		sql.append("`body`= ?;", body);
+		
 		int id = DBUtil.insert(conn, sql);
+
 		return id;
 	}
+
 
 }
--- src/com/KoreaIT/java/JDBCAM/service/ArticleService.java
+++ src/com/KoreaIT/java/JDBCAM/service/ArticleService.java
@@ -3,17 +3,21 @@ package com.KoreaIT.java.JDBCAM.service;
 import java.sql.Connection;
 
 import com.KoreaIT.java.JDBCAM.dao.ArticleDao;
-import com.KoreaIT.java.JDBCAM.util.DBUtil;
-import com.KoreaIT.java.JDBCAM.util.SecSql;
 
 public class ArticleService {
-	ArticleDao articleDao = new ArticleDao();
+	
+	private ArticleDao articleDao;
 
-	public int dbInsert(Connection conn, SecSql sql) {
-		
-		int id = articleDao.dbInsert(conn, sql);
-		
-		return id;
+	public ArticleService(Connection conn) {
+		this.articleDao = new ArticleDao(conn);
 	}
+	
+	
+	public int doWrite(String title, String body) {
+		
+		return articleDao.doWrite(title, body);
+	}
+	
+	
 
 }
